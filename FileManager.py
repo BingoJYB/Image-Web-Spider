e586b48b5150163191a5d9d64bbb321348ff27f2
BingoJYB <7303639+BingoJYB@users.noreply.github.com>
2018-07-15 13:32:17 +0200

    Manage the input file with image URLs

--- FileManager.py
+++ FileManager.py
@@ -1,8 +1,11 @@
 class FileManager(object):
+    
     """
     This is a class of managing the input plain file.
     """
+    
     def __init__(self, input_file_path, url_chunk_size):
+        
         """
         This is a constructor for class FileManager.
 
@@ -10,10 +13,12 @@ class FileManager(object):
             input_file_path: The path of the input file.
             url_chunk_size: The size of the chunk of URLs used to download once.
         """
+        
         self.input_file_path = input_file_path
         self.url_chunk_size = url_chunk_size
 
     def read_file(self, input_file):
+        
         """
         This is a function of reading image URLs in the plain text by the chunk.
 
@@ -23,6 +28,7 @@ class FileManager(object):
         Returns:
             Return a generator.
         """
+        
         lines = []
         while True:
             line = input_file.readline().strip()
@@ -37,6 +43,7 @@ class FileManager(object):
                 lines = []
 
     def get_image_url(self):
+        
         """
         This is a function of getting image URLs.
 
@@ -46,6 +53,7 @@ class FileManager(object):
         Raises:
             IOError: File IO error.
         """
+        
         try:
             image_url_file = open(self.input_file_path, "r")
             url_generator = self.read_file(image_url_file)
